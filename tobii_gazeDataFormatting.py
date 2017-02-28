68e0ef5cb207592ef1e60ec4150fa905aee41a29
Jeff MacInnes <jeff.macinnes@gmail.com>
2017-02-27 23:40:09 -0500

    data formatting

---
+++ tobii_gazeDataFormatting.py
@@ -0,0 +1,184 @@
+"""
+Tools for formatting the gaze data from a Tobii Glasses 2 recording
+"""
+import os, sys, shutil
+from io import open
+import csv
+from os.path import join
+from itertools import chain
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import json
+import gzip
+import cv2
+
+def copyTobiiRecording(input_dir, output_root):
+	"""
+	Copy the relevant files from the specified input dir to the specified output dir
+	"""
+
+	# read the data and creation time from the segment.json file
+	with open(join(input_dir, 'segment.json')) as j:
+		segData = json.loads(j.read())
+	createDate = segData['seg_created']
+	dt = datetime.strptime(createDate, "%Y-%m-%dT%H:%M:%S+0000") 
+
+	# create a new output directory based on creation date
+	date_dir = dt.strftime('%Y_%m_%d')
+	time_dir = dt.strftime('%H-%M-%S')
+	if not os.path.isdir(join(output_root, date_dir, time_dir)):
+		os.makedirs(join(output_root, date_dir, time_dir))
+	outputDir = join(output_root, date_dir, time_dir)
+
+	# Copy relevent files to new directory 
+	for f in ['livedata.json.gz', 'fullstream.mp4']:
+		shutil.copyfile(join(input_dir, f), join(outputDir, f))
+
+	# Unzip the gaze data file
+	with gzip.open(join(outputDir, 'livedata.json.gz')) as zipFile:
+		with open(join(outputDir, 'livedata.json'), 'wb') as unzippedFile:
+			for line in zipFile:
+
+				unzippedFile.write(line)
+	os.remove(join(outputDir, 'livedata.json.gz'))
+
+	# return the full path to the output dir
+	return outputDir
+
+
+def formatGazeData(input_dir):
+	"""
+	load livedata.json, write to csv
+	format to get the gaze coordinates w/r/t world camera, and timestamps for every frame of video
+	""" 
+
+	# convert the json file to pandas dataframe
+	gaze_df = json_to_df(join(input_dir, 'livedata.json'))
+	gaze_df.to_csv(join(input_dir, 'gazeData.csv'), sep='\t')
+
+	## read video file, create array of frame timestamps
+	frame_timestamps = getVidFrameTimestamps(join(input_dir, 'fullstream.mp4'))
+	print(frame_timestamps)
+
+
+def getVidFrameTimestamps(vid_file):
+	"""
+	Load the supplied video, return an array of frame timestamps
+	"""
+	
+
+	OPENCV3 = (cv2.__version__.split('.')[0] == '3')		# get opencv version
+
+	vid = cv2.VideoCapture(vid_file)
+	if OPENCV3:
+		totalFrames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+	else:
+		totalFrames = vid.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
+
+	print('{} total frames'.format(int(totalFrames)))
+	frame_ts = np.zeros(int(totalFrames))
+
+	# loop through all video frames
+	frameCounter = 0
+	while vid.isOpened():
+		# read the next frame
+		ret, frame = vid.read()
+
+		# check if its valid:
+		if ret == True:
+			if OPENCV3:
+				thisFrameTS = vid.get(cv2.CAP_PROP_POS_MSEC)
+			else:
+				thisFrameTS = vid.get(cv2.cv.CV_CAP_PROP_POS_MSEC)
+			
+			# write this frame's ts to the array
+			frame_ts[frameCounter] = thisFrameTS
+
+			# increment frame counter
+			frameCounter += 1
+
+		else:
+			break
+	vid.release()		# close the video
+
+	return frame_ts
+
+
+def json_to_df(json_file):
+	"""
+	convert the livedata.json file to a pandas dataframe
+	"""
+	# dicts to store sync points
+	vts_sync = {}			# RECORDED video timestamp sync
+	df = pd.DataFrame()     # empty dataframe to write data to 
+
+	with open(json_file, 'rb') as j:
+
+		# loop over all lines in json file, each line represents unique json object
+		for line in j:
+			entry = json.loads(line)
+
+			# check status of this row
+			if entry['s'] != 0:
+				continue
+
+			### a number of different dictKeys are possible, respond accordingly
+			elif 'vts' in entry.keys(): # "vts" key signfies a video timestamp (first frame, first keyframe, and ~1/min afterwards)
+				vts_sync[entry['ts']] = entry['vts']
+				continue
+
+			# if this json object contains "eye" data (e.g. pupil info) 
+			if 'eye' in entry.keys():
+				which_eye = entry['eye'][:1]
+				if 'pc' in entry.keys():
+					df.loc[entry['ts'], which_eye + '_pup_cent_x'] = entry['pc'][0]
+					df.loc[entry['ts'], which_eye + '_pup_cent_y'] = entry['pc'][1]
+					df.loc[entry['ts'], which_eye + '_pup_cent_z'] = entry['pc'][2]
+					df.loc[entry['ts'], which_eye + '_pup_cent_val'] = entry['s']
+				elif 'pd' in entry.keys():
+					df.loc[entry['ts'], which_eye + '_pup_diam'] = entry['pd']
+					df.loc[entry['ts'], which_eye + '_pup_diam_val'] = entry['s']
+				elif 'gd' in entry.keys():
+					df.loc[entry['ts'], which_eye + '_gaze_dir_x'] = entry['gd'][0]
+					df.loc[entry['ts'], which_eye + '_gaze_dir_y'] = entry['gd'][1]
+					df.loc[entry['ts'], which_eye + '_gaze_dir_z'] = entry['gd'][2]
+					df.loc[entry['ts'], which_eye + '_gaze_dir_val'] = entry['s']
+			
+			# otherwise it contains gaze position data
+			else:
+				if 'gp' in entry.keys():
+					df.loc[entry['ts'], 'gaze_pos_x'] = entry['gp'][0]
+					df.loc[entry['ts'], 'gaze_pos_y'] = entry['gp'][1]
+					df.loc[entry['ts'], 'gaze_pos_val'] = entry['s']
+				elif 'gp3' in entry.keys():
+					df.loc[entry['ts'], '3d_gaze_pos_x'] = entry['gp3'][0]
+					df.loc[entry['ts'], '3d_gaze_pos_y'] = entry['gp3'][1]
+					df.loc[entry['ts'], '3d_gaze_pos_z'] = entry['gp3'][2]
+					df.loc[entry['ts'], '3d_gaze_pos_val'] = entry['s']
+
+		# set video timestamps column
+		df['vts_time'] = np.array(df.index)	   # df.index is data timstamps
+		df.ix[df.index < min(sorted(vts_sync.keys())), 'vts_time'] = np.nan		# set rows that occur before the first frame to nan
+		
+		# for each new vts sync package, reindex all of the rows above that timestamp
+		for key in sorted(vts_sync.keys()):
+			df.ix[df.index >= key, 'vts_time'] = np.array(df.index)[df.index >= key]   # necessary if there are more than 2 keys in the list (prior key changes need to be reset for higher vts syncs)
+			df.ix[df.index >= key, 'vts_time'] = df.vts_time - key + vts_sync[key]
+
+		# note: the vts column indicates, in microseconds, where this datapoint would occur in the video timeline 
+		# these do NOT correspond to the timestamps of when the videoframes were acquired. Need cv2 methods for that. 
+
+		# add seconds column
+		df = df.reset_index()
+		df['seconds'] = (df['index'] - df['index'][0]) / 1000000.0		# convert tobii ts (us) to seconds
+		df = df.set_index('index', drop=True)
+
+		# return the dataframe
+		return df
+
+
+
+
+
+
--- tobii_processRecording.py
+++ tobii_processRecording.py
@@ -24,5 +24,95 @@ import pandas as pd
 import argparse
 import time
 
+from tobii_gazeDataFormatting import copyTobiiRecording, formatGazeData
+
 OPENCV3 = (cv2.__version__.split('.')[0] == '3')
 print("OPENCV version " + cv2.__version__)
+
+
+"""
+This script can be called to process recordings by Tobii Glasses 2
+
+Usage:
+	python tobii_processRecording.py <path to input dir> <path to output directory> <path to reference stimulus> <path to camera calibration JSON>
+
+Inputs:
+	- recording directory: path to recording session directory (either raw data on SD card, or a local copy of that data on harddrive)
+	- output directory: path to where you want the data copied to and where the 'processed' results written
+	- reference stimulus: path to a decently high resolution jpg of the stimulus you are attempting to map gaze on to
+	- camera calibration: path to the camera calibration JSON file for this make/model of eye-tracker
+
+Outputs:
+	- all outputs written to "processed" directory created within the specified output directory
+	- movies (all output movies for the specified frames only):
+		- orig
+		- orig + gaze overlay
+		- orig + evolving heatmap overlay
+		- ref + evolving heatmap overlay
+	- data:
+		- camGazePosition: camera locations
+		- camGazePosition_smooth: spike removal and smoothing
+
+Note about coordinates:
+The operations below focus on translating between multiple different coordinate systems.
+For clarity, here are those different systems and the labels used when referencing each:
+	- World Frame (world):		Coordinate system used on frames take from the camera (e.g. world camera on glasses)
+								This is a 2D system with the origin in the bottom-left corner of the frame, units in pixels
+	- Reference Image (ref):	Coordinate system of the reference image that is expected to be found in the camera frame. 
+								This is a 2D system with the origin in the top-left corner, units in pixels
+	- Object (obj):				Coordinate system of the reference image AS IT OCCURS IN THE PHYSICAL WORLD. This is a 3D
+								system, however all z-values are set to 0, defining the plane of the wall that the image is 
+								on. The units are in whatever physical coordinates you desire (e.g. inches); whatever unit you 
+								set will be used on subsequent measurements, such as camera position. 
+"""
+
+
+def processRecording(inputDir, refFile, cameraCalib):
+	"""
+	Open the recording from the input dir
+	Format the pupil data, 
+	Loop through each frame of the recording and create output videos
+	"""
+	# Settings:
+	framesToUse = np.arange(0, 445, 1)
+
+	# start time
+	process_startTime = time.time()
+
+	# specify an output dir for the 'processed' results
+	outputDir = join(inputDir, 'processed')
+	if not os.path.isdir(outputDir):
+		os.makedirs(outputDir)
+
+	### Prep the gaze data ################################
+	print('Prepping gaze data...')
+	# format pupil data
+	formatGazeData(inputDir)
+
+
+
+
+###
+if __name__ == '__main__':
+	# parse arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument('inputDir', help='path to the raw recording dir (e.g. SD card)')
+	parser.add_argument('outputDir', help='path to where output data copied and saved to')
+	parser.add_argument('referenceFile', help='path to reference stimuli')
+	parser.add_argument('cameraCalibration', help='path to camera calibration file')
+	args = parser.parse_args()
+
+	# Check if input directory is valid
+	if not os.path.isdir(args.inputDir):
+		print('Invalid input dir: ' + args.inputDir)
+		sys.exit()
+	else:
+		# copy the raw data to the output dir
+		newDataDir = copyTobiiRecording(args.inputDir, args.outputDir)
+
+		# processing the data (note: the output dir from previous step is now the input dir for this step)
+		processRecording(newDataDir, args.referenceFile, args.cameraCalibration)
+
+
+
+
